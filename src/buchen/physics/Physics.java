cc671b47a71ba29635576aeafe00d59fc6a1bbcf
Ayelet Buchen <ayeletbuchen@Ayelets-MacBook-Air.local>
2019-02-05 20:08:59 -0500

    fixed constants and added braces

--- src/buchen/physics/Physics.java
+++ src/buchen/physics/Physics.java
@@ -7,24 +7,31 @@ public class Physics {
 
     public static void main(String[] args) {
 
-        final int velocityMin = 50, velocityMax = 120, degreesMin = 30, degreesMax = 150, seconds = 120, projectileBound = 6;
+        final int VELOCITY_MIN = 50;
+        final int VELOCITY_MAX = 120;
+        final int DEGREES_MIN = 30;
+        final int DEGREES_MAX = 150;
+        final int SECONDS = 120;
+        final int PROJECTILE_BOUND = 6;
         Random random = new Random();
         ArrayList<Projectile> projectiles = new ArrayList<>();
 
-        for (int i = 0; i < seconds; i++) {
+        for (int i = 0; i < SECONDS; i++) {
             for (Projectile p : projectiles) {
                 p.addTime(1);
             }
-            int nrProjectiles = random.nextInt(projectileBound);
+            int nrProjectiles = random.nextInt(PROJECTILE_BOUND);
             for (int j = 0; j < nrProjectiles; j++) {
-                double velocity = (velocityMax - velocityMin) * random.nextDouble() + velocityMin;
-                double degrees = (degreesMax - degreesMin) * random.nextDouble() + degreesMin;
+                double velocity = (VELOCITY_MAX - VELOCITY_MIN) * random.nextDouble() + VELOCITY_MIN;
+                double degrees = (DEGREES_MAX - DEGREES_MIN) * random.nextDouble() + DEGREES_MIN;
                 projectiles.add(new Projectile(velocity, degrees));
             }
         }
 
         for (Projectile p : projectiles) {
-            if (p.getY() > 0) System.out.println(p);
+            if (p.getY() > 0) {
+                System.out.println(p);
+            }
         }
     }
 }
